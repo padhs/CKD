eb138416677eedcd9011a98a2df7a716d3fc542f
padhs <arosekpadhi@gmail.com>
2024-04-23 14:45:15 +0530

    eda

--- EDA_CKD.py
+++ EDA_CKD.py
@@ -3,6 +3,11 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./datasets/chronic_kidney_disease_full.csv')
+df = pd.read_csv('./datasets/chronic_kidney_disease_EDA.csv')
 
 # Exploratory Data Analysis -->
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
+
+
